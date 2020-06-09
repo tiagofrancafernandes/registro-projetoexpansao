fca9132f110623ba0d39e4581475e9217f2d2672
França Tiago <tiago.fernandes@pontomais.com.br>
2020-06-09 15:39:21 -0300

    Alterando footer

--- resources/views/layouts/registro/includes/footer.blade.php
+++ resources/views/layouts/registro/includes/footer.blade.php
@@ -1,12 +1,12 @@
-<footer class="footer">
+<footer class="footer bg-dark text-light">
     <div class="d-md-flex flex-md-column justify-content-md-center align-items-md-center h-25">
         <div class="row text-center mt-3 pt-3 p-2">
             <div class="col-md-12">
-                <span class="text-muted justify-content-md-center "> Desenvolvido por <a class="font-weight-bold" href="http://tiagofranca.com" target="_blank">Tiago França</a></span> para
-                <span class="text-muted justify-content-md-center "><a class="font-weight-bold" href="http://projetoexpansao.ong.br" target="_blank">projetoexpansao.ong.br</a></span>
+                <span class="text-light justify-content-md-center "> Desenvolvido por <a class="font-weight-bold" href="http://tiagofranca.com" target="_blank">Tiago França</a></span> para
+                <span class="text-light justify-content-md-center "><a class="font-weight-bold" href="http://projetoexpansao.ong.br" target="_blank">projetoexpansao.ong.br</a></span>
             </div>
             <div class="col-md-12">
-                <span class="text-muted">{{ __("All rights reserved.") }} &copy; <span class="font-weight-bold">{{ date('Y') }}</span>
+                <span class="text-light">{{ __("All rights reserved.") }} &copy; <span class="font-weight-bold">{{ date('Y') }}</span>
             </div>
         </div>
     </div>
