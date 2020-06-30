34c6e1b023bae772bc80458ad57247c2336b96a4
França Tiago <tiago.fernandes@pontomais.com.br>
2020-06-30 17:24:00 -0300

    Alterando a apresentaçao do rodape/copyright

--- resources/views/layouts/registro/includes/footer.blade.php
+++ resources/views/layouts/registro/includes/footer.blade.php
@@ -1,7 +1,7 @@
  {{--  <footer class="footer bg-dark text-light mt-3 pt-3 footer_registro">  --}}
-<footer class="footer bg-dark text-light mt-3 pt-3 footer_registro footer fixed-bottom footer-light footer-shadow content container-fluid">
+<footer class="footer bg-dark text-light pt-1 footer_registro footer fixed-bottom footer-light footer-shadow content container-fluid">
     <div class="d-md-flex flex-md-column justify-content-md-center align-items-md-center h-25">
-        <div class="row text-center mt-3 pt-3 p-2">
+        <div class="row text-center copy_footer">
             <div class="col-md-12">
                 <span class="text-light justify-content-md-center "> Desenvolvido por <a class="font-weight-bold" href="http://tiagofranca.com" target="_blank">Tiago França</a></span> para
                 <span class="text-light justify-content-md-center "><a class="font-weight-bold" href="http://projetoexpansao.ong.br" target="_blank">projetoexpansao.ong.br</a></span>
